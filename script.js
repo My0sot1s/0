9b5454b104ffc447a09b69374d37bba0eb71a339
My0sot1s <1049396729@qq.com>
2021-04-12 20:04:22 +0800

    增加了ai先手选项

--- script.js
+++ script.js
@@ -5,42 +5,50 @@ const cells=document.querySelectorAll(".cell");
 var r = Array.from(cells);
 var GAMEOVER = 0;
 var arr= new Array(18);
-var x=4;
+arr[7]=1919810;    //用于确定平局
+arr[8]=1919810;
+arr[9]=1919810;
+var x=4;    
+var y=9;
 var YourScore=0;
 var ComputerScore=0;
-console.log(arr);
 
 //计分板
 document.querySelector("#title1").innerHTML="您得分"+YourScore;
 document.querySelector("#title2").innerHTML="电脑得分"+ComputerScore;
 
+
 //玩家落子
 var count1=0;
 var arr1= new Array(9);
 var exit= false;
-arr1[3]=114514;
+arr1[3]=114514;    //用于确定平局
 arr1[4]=114514;
+arr1[5]=114514;
 function getEventTrigger(event){
-	const A=event.target.id;
+	var A=event.target.id;
+	A=parseInt(A);
 	if(GAMEOVER==0){    //游戏结束之后无法落子
 	arr[17]=1919810;  //防止array some()方法不检测空数组而报错
-		console.log(A);
+		console.log("A="+A);
 		console.log(arr);
-	if(arr.some(function(element,index,array){    //避免在同一位置落子
+	arr.some(function(element,index,array){
 		if (element==A){
+			alert("请勿重复落子！");
+			window.location.reload()
 			return false;
 		}
-		return true;
-	}))
-	{cells[A].innerText=PLAYER;    
+		return true;		
+	})
+	{cells[A].innerText=PLAYER; 
 	r[A]=1;
 	console.log(arr1);
 	arr1[count1]=A;
 	count1++;
-	console.log('count1='+count1);
 	checkWin();
-	
-	console.log(arr1[4]);
+	console.log(arr1);
+	console.log("exit"+exit);
+	console.log(arr1[x]);
 	if(exit==false&&arr1[x]==114514){    //防止游戏结束后AI继续落子,arr长度为8时不执行AI函数（*1）
 	ai(arr1)
 	}
@@ -52,13 +60,11 @@ var count2=0;
 var index=0;
 var arr2= new Array(9);
 function ai(arr1){
+	if(GAMEOVER==0){
 	do{
 		var B=0;
 	    var B=Math.floor(Math.random()*9);
-		console.log(arr1);
-		console.log('B='+B);
 		arr[index]=arr1[count2];
-		
 	}while(arr.some(function(element,index,array){    //实现在棋盘空白处落子;当arr长度为8时死循环,利用(*1)跳出
 		if (element==B){
 			return true;
@@ -68,16 +74,14 @@ function ai(arr1){
 	cells[B].innerText=AIPLAYER;    
 	r[B]=2;
 	arr2[count2]=B;
-	arr[index+1]=arr2[count2]
+	arr[index+1]=arr2[count2];
 	count2++;
 	index=index+2;
-	console.log('count2='+count2);
 	console.log(arr);
+	console.log(arr1);
 	checkWin();
-	
 }
-
-console.log(r);
+}
 
 //赢法 简单粗暴
 function checkWin(){
@@ -115,13 +119,13 @@ function checkWin(){
             document.querySelector("#title2").innerHTML="电脑得分"+ComputerScore;
 		}
 	else if(
-	   arr1[4]!=114514){
+	   arr[y]!=1919810){
 	   GAMEOVER=1;
 	   exit=true;
 	   window.setTimeout("alert('平局')",1);
 	}
 	else{
-		arr1[4]=114514;	//
+		arr1[4]=114514;	
 	}
 }
 
@@ -132,13 +136,33 @@ function reStart(){
 		r[i]=0;
 	}
 	    arr=new Array(18);
+	    arr[7]=1919810;
+	    arr[8]=1919810;
+	    arr[9]=1919810;
 		exit=false;
 		arr1= new Array(9);
 	    arr1[4]=114514;
+	    arr1[5]=114514;
 		arr2= new Array(9);
 		count1=0;
 		count2=0;
+	    index=0;
 		B=0;
+	    x=4;
+	    y=9;
 	    GAMEOVER=0;
 		console.log(arr1);
+}
+
+//人机先手
+function aiFirst(){
+	if(GAMEOVER==0){
+	var C=Math.floor(Math.random()*9);
+	cells[C].innerText=AIPLAYER; 
+	r[C]=2;
+	x=5;          //改变平局条件
+	y=7;
+	arr2[0]=C;
+	arr[16]=C;    //让array some()方法实现
+	}
 }
